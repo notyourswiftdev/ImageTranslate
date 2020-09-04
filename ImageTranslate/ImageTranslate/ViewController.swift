9211bf4666bc3c51d0cca06907a12fdd138dd4f8
Aaron <toscleveland@gmail.com>
2020-09-03 19:29:30 -0500

    Init Project

---
+++ ImageTranslate/ImageTranslate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageTranslate
+//
+//  Created by Aaron Cleveland on 9/3/20.
+//  Copyright © 2020 Aaron Cleveland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
